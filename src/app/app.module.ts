34b9887db8d6a7475cb3d85d5b08d63930013756
Pushkar JK <pushkarjk@gmail.com>
2020-10-27 09:53:32 -0500

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TravelComponent } from './travel/travel.component';
+
+
+const routes: Routes = [
+  {path: 'todo', component:TravelComponent},
+  {path: '', redirectTo: '/todo', pathMatch: 'full'}
+];
+
 
 @NgModule({
   declarations: [
